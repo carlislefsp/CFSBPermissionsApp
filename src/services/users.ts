5154fc67be6eed41237024be097835ba902af190
Chad <elmofromok@gmail.com>
2025-02-24 12:48:34 -0600

    Enhance user service with group permissions 🎉
    - Added methods to fetch user groups and their permissions.
    - Implemented functionality to assign and remove permissions for user groups.
    - Cleaned up endpoint usage for better clarity.

--- src/services/users.ts
+++ src/services/users.ts
@@ -1,15 +1,63 @@
-import { User } from '@/types/user';
+import { User, GroupUserPermission } from '@/types/user';
+import { Group } from '@/types/group';
+import { Permission } from '@/types/permission';
 import { ApiService } from './api';
 
 export class UserService extends ApiService {
-  private static readonly USERS_ENDPOINT = '/users/';
-
   static async getUsers(): Promise<User[]> {
-    return this.fetch<User[]>(this.USERS_ENDPOINT);
+    return this.fetch<User[]>('/users');
   }
 
-  // Add other user-related methods
-  static async getUserById(id: string): Promise<User> {
-    return this.fetch<User>(`${this.USERS_ENDPOINT}${id}`);
+  static async getUserById(oid: string): Promise<User> {
+    return this.fetch<User>(`/users/${oid}`);
+  }
+
+  static async getUserGroups(oid: string): Promise<Group[]> {
+    return this.fetch<Group[]>(`/users/${oid}/groups`);
+  }
+
+  static async getUserGroupPermissions(
+    oid: string,
+    groupId: string,
+  ): Promise<Permission[]> {
+    return this.fetch<Permission[]>(
+      `/users/${oid}/groups/${groupId}/permissions`,
+    );
+  }
+
+  static async getUserGroupPermission(
+    oid: string,
+    groupId: string,
+    permissionId: string,
+  ): Promise<GroupUserPermission> {
+    return this.fetch<GroupUserPermission>(
+      `/users/${oid}/groups/${groupId}/permissions/${permissionId}`,
+    );
+  }
+
+  static async assignPermissionToUserGroup(
+    oid: string,
+    groupId: string,
+    permissionId: string,
+  ): Promise<GroupUserPermission> {
+    return this.fetch<GroupUserPermission>(
+      `/users/${oid}/groups/${groupId}/permissions/${permissionId}`,
+      {
+        method: 'POST',
+      },
+    );
+  }
+
+  static async removePermissionFromUserGroup(
+    oid: string,
+    groupId: string,
+    permissionId: string,
+  ): Promise<void> {
+    await this.fetch(
+      `/users/${oid}/groups/${groupId}/permissions/${permissionId}`,
+      {
+        method: 'DELETE',
+      },
+    );
   }
 }
